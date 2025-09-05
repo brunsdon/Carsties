6e407570a42fec3c1c3d65bbb9ad3dbd666da141
Matthew Brunsdon <brunsdon@engineer.com>
2025-09-05 12:11:10 +1000

    End of section 13

---
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+type Props = {
+    auctionId: string;
+    highBid: number;
+}
+
+import { placeBidForAuction } from '@/app/actions/auctionActions';
+import { useBidStore } from '@/hooks/useBidStore';
+import { numberWithCommas } from '@/lib/numberWithComma';
+import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
+export default function BidForm({ auctionId, highBid }: Props) {
+    const { register, handleSubmit, reset } = useForm();
+    const addBid = useBidStore(state => state.addBid);
+
+    function onSubmit(data: FieldValues) {
+        if (data.amount <= highBid) {
+            reset();
+            return toast.error('Bid must be at least $' + numberWithCommas(highBid + 1));
+        }
+        placeBidForAuction(auctionId, +data.amount).then(bid => {
+            if (bid.error) {
+                reset();
+                throw bid.error;
+            }
+            addBid(bid);
+            reset();
+        }).catch(err => toast.error(err.message))
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='flex items-center border-2 rounded-lg py-2'>
+            <input
+                type="number"
+                {...register('amount')}
+                className='flex-grow pl-5 bg-transparent focus:outline-none border-transparent forcus:border-transparent focus:ring-0 text-sm text-gray-600'
+                placeholder={`Enter your bid  (minimum bid is $${numberWithCommas(highBid)})`}
+            />
+        </form>
+    )
+}
